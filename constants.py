b9c05794a511d04ee075dee5805dc9ebd41d5707
Zaida Hammel <zzaida.hammel@mths.ca>
2022-10-03 14:32:54 +0000

    added pizza.py

---
+++ constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Created by: Zaida Hammel
+# Created on: Oct 2022
+# This module contains constants for the pizza program
+LABOR = 0.75
+RENT = 1.00
+COST_PER_INCH = 0.50
+HST = 0.13
---
+++ pizza.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Created by: Zaida Hammel
+# Created on: Oct 2022
+# This program calculates the cost of pizza
+import constants
+
+def main():
+    # this function calculates the cost of pizza
+    # input
+    diameter = int(
+        input("Enter the diameter of the pizza you would " + "like (inch): ")
+    )
+    # process
+    sub_total = (
+        constants.LABOR + constants.RENT + (diameter * constants.COST_PER_INCH)
+    )
+    total = sub_total + (sub_total * constants.HST)
+    # output
+    print("")
+    print("The cost for a {0} inch pizza is ${1:,.2f}".format(diameter, total))
+    print("\nDone.")
+
+if __name__ == "__main__":
+    main()
