374c5dabc7b5aa6ae15aa50e82f8807b07292b1e
Zaida Hammel <zzaida.hammel@mths.ca>
2022-10-03 14:47:25 +0000

    edited formattin

--- pizza.py
+++ pizza.py
@@ -4,6 +4,7 @@
 # This program calculates the cost of pizza
 import constants
 
+
 def main():
     # this function calculates the cost of pizza
     # input
@@ -11,14 +12,13 @@ def main():
         input("Enter the diameter of the pizza you would " + "like (inch): ")
     )
     # process
-    sub_total = (
-        constants.LABOR + constants.RENT + (diameter * constants.COST_PER_INCH)
-    )
+    sub_total = constants.LABOR + constants.RENT + (diameter * constants.COST_PER_INCH)
     total = sub_total + (sub_total * constants.HST)
     # output
     print("")
     print("The cost for a {0} inch pizza is ${1:,.2f}".format(diameter, total))
     print("\nDone.")
+
 
 if __name__ == "__main__":
     main()
